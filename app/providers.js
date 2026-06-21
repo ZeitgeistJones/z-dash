9cc12a6a14ad2e9140a1813af86b291acde7cd8b
Zeitgeist Jones <teachervin77777@gmail.com>
2026-06-20 21:29:33 -0400

    Create providers.js

---
+++ app/providers.js
@@ -0,0 +1,23 @@
+"use client";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { base } from "wagmi/chains";
+import { injected } from "wagmi/connectors";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const config = createConfig({
+  chains: [base],
+  connectors: [injected()],
+  transports: {
+    [base.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
+
+export default function Providers({ children }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
