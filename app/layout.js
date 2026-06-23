cd9220be0b71103511c94f62eb58bda721acd1ab
Zeitgeist Jones <teachervin77777@gmail.com>
2026-06-23 00:06:21 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning style={{ background: "#1c1b22" }}>
       <head>
         <script
           dangerouslySetInnerHTML={{
